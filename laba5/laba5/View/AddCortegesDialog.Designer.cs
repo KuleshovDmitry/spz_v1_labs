cf9256a57a064f216911a90dc5822090f108c85e
KuleshovDmitry <dmytro.kulishov@nure.ua>
2018-11-15 22:07:03 +0200

    added MVC separation

--- laba5/laba5/View/AddCortegesDialog.Designer.cs
+++ laba5/laba5/View/AddCortegesDialog.Designer.cs
@@ -34,7 +34,6 @@
             this.disciplinesTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // CloseButton
@@ -76,35 +75,24 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(225, 13);
+            this.label1.Size = new System.Drawing.Size(122, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Пишите названия дисциплин через пробел";
+            this.label1.Text = "Название дисциплины";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(238, 13);
+            this.label2.Size = new System.Drawing.Size(109, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Пишите имена преподавателей через пробел";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F);
-            this.label3.Location = new System.Drawing.Point(9, 142);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(209, 7);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "каждый добавленный преподаватель ведет каждый предмет";
+            this.label2.Text = "Имя преподавателя";
             // 
             // AddCortegesDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(267, 163);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.disciplinesTextBox);
@@ -126,6 +114,5 @@
         private System.Windows.Forms.TextBox disciplinesTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
     }
 }
