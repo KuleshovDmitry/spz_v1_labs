940dab364121064de0431362e2ec7e0ca05a5d0e
KuleshovDmitry <dmytro.kulishov@nure.ua>
2018-11-15 14:23:52 +0200

    loaded laba4

---
+++ laba4/laba4/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace laba4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.saveFileButton = new System.Windows.Forms.Button();
+            this.createRandomFileButton = new System.Windows.Forms.Button();
+            this.saveAsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "in files (*.in)|*.in|txt files (*.txt)|*.txt";
+            this.openFileDialog.FilterIndex = 2;
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "out (*.out)|*.in|txt (*.txt)|*.txt";
+            this.saveFileDialog.FilterIndex = 2;
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog_FileOk);
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(12, 142);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(153, 23);
+            this.selectFileButton.TabIndex = 0;
+            this.selectFileButton.Text = "открыть";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.selectFileButton_Click);
+            // 
+            // saveFileButton
+            // 
+            this.saveFileButton.Location = new System.Drawing.Point(175, 113);
+            this.saveFileButton.Name = "saveFileButton";
+            this.saveFileButton.Size = new System.Drawing.Size(115, 23);
+            this.saveFileButton.TabIndex = 1;
+            this.saveFileButton.Text = "сохранить";
+            this.saveFileButton.UseVisualStyleBackColor = true;
+            this.saveFileButton.Click += new System.EventHandler(this.saveFileButton_Click);
+            // 
+            // createRandomFileButton
+            // 
+            this.createRandomFileButton.Location = new System.Drawing.Point(12, 113);
+            this.createRandomFileButton.Name = "createRandomFileButton";
+            this.createRandomFileButton.Size = new System.Drawing.Size(153, 23);
+            this.createRandomFileButton.TabIndex = 2;
+            this.createRandomFileButton.Text = "создать слчайный файл";
+            this.createRandomFileButton.UseVisualStyleBackColor = true;
+            this.createRandomFileButton.Click += new System.EventHandler(this.createRandomFileButton_Click);
+            // 
+            // saveAsButton
+            // 
+            this.saveAsButton.Location = new System.Drawing.Point(175, 142);
+            this.saveAsButton.Name = "saveAsButton";
+            this.saveAsButton.Size = new System.Drawing.Size(115, 23);
+            this.saveAsButton.TabIndex = 3;
+            this.saveAsButton.Text = "сохранить как";
+            this.saveAsButton.UseVisualStyleBackColor = true;
+            this.saveAsButton.Click += new System.EventHandler(this.saveAsButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 177);
+            this.Controls.Add(this.saveAsButton);
+            this.Controls.Add(this.createRandomFileButton);
+            this.Controls.Add(this.saveFileButton);
+            this.Controls.Add(this.selectFileButton);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.Button saveFileButton;
+        private System.Windows.Forms.Button createRandomFileButton;
+        private System.Windows.Forms.Button saveAsButton;
+    }
+}
+
---
+++ laba4/laba4/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace laba4
+{
+    public partial class MainForm : Form
+    {
+        StreamReader streamReader;
+        string fileName;
+        int count = 0;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            streamReader = new StreamReader(openFileDialog.FileName);
+            doCounting();
+            fileName = openFileDialog.FileName;
+        }
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+        private void saveFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            File.WriteAllText(saveFileDialog.FileName, count.ToString());
+        }
+        private void doCounting()
+        {
+            try
+            {
+                int[] arrayInt = streamReader.ReadToEnd().Split(' ').Select(n => int.Parse(n)).ToArray();
+                for (int i = 1; i < arrayInt.Length; i++)
+                {if (arrayInt[i - 1] != arrayInt[i])
+                    {count++;}
+                }
+            }
+            catch (FormatException) { MessageBox.Show("ошибка: файл должен содержать только целые числа"); }
+            streamReader.Close();
+        }
+        private void saveFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(File.Create(@"../../" + Path.GetFileNameWithoutExtension(fileName) + ".out"));
+                streamWriter.WriteLine(count.ToString());
+                streamReader.Close();
+                streamWriter.Close();
+            }
+            catch (NullReferenceException) { }
+        }
+        private void saveAsButton_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.ShowDialog();
+            streamReader.Close();
+        }
+        private void createRandomFileButton_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            fileName = (@"../../" + rnd.Next(9999).ToString() + ".in");
+            StreamWriter streamWriter = new StreamWriter(File.Create(fileName));
+            {
+                int numberIteration = rnd.Next(100);
+                for (int i = 0; i < numberIteration; i++)
+                {streamWriter.Write((rnd.Next(i % 10)).ToString());
+                    if (i != numberIteration - 1)
+                        streamWriter.Write(' ');}
+            }
+            streamWriter.Close();
+            streamReader = new StreamReader(fileName);
+            doCounting();
+        }
+    }
+}
